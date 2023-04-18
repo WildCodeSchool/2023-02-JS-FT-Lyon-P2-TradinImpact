ed067e609e1e305dd2ac21b5e18b931a97193244
Guillaume Lamourec <parkerpool@live.fr>
2023-04-18 22:59:06 +0200

    created the header component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import GameScreen from "../components/GameScreen";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const [moraCount, setMoraCount] = useState(20);
+
   return (
     <>
-      <Header />
+      <Header moraCount={moraCount} setMoraCount={setMoraCount} />
       <GameScreen />
       <Footer />
     </>
