b8c44a69251f8f31b62ea6b2d3e7d8633a04e6ad
Yann Richard <yann.richard9@gmail.com>
2023-04-19 11:44:11 +0200

    adding new icons, and fixing display for mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Start from "./pages/Start";
-import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
