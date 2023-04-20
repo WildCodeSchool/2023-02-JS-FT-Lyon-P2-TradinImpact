603df36a5b362af5d830099b6a84946cc80bb47d
Francois Chavallard <francois.chavallard@gmail.com>
2023-04-20 15:36:14 +0200

    création du composant Sell et de ses sous-composants + révision du système de route et ajout d'un state pour le game mode et d'un state pour les écrans de navigation dans Trade

---
+++ frontend/src/components/TradeItemDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TradeItemDisplay() {
+  return <div>TradeItemDisplay</div>;
+}
+
+export default TradeItemDisplay;
---
+++ frontend/src/components/TradeMenu.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TradeMenu() {
+  return <div>TradeMenu</div>;
+}
+
+export default TradeMenu;
