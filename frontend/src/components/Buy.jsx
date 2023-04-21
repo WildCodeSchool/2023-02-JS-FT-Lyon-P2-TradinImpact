7ec540629880de7bfdd0121fa5a4e14d2a1ec640
Yann Richard <yann.richard9@gmail.com>
2023-04-21 10:56:27 +0200

    adding the code for the buy component (not yet functional)

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -1,3 +1,59 @@
+import React, { useState } from "react;";
+
 export default function Buy() {
-  return <h1>C'est le composant Buy</h1>;
+  const processedItems = [
+    "chilled-meat",
+    "flour",
+    "milk",
+    "pepper",
+    "rice",
+    "salt",
+    "shrimp-meat",
+    "butter",
+    "cream",
+    "ham",
+    "smoked-fowl",
+    "sugar",
+    "bacon",
+    "cheese",
+    "crab-roe",
+    "jam",
+    "sausage",
+  ];
+
+  function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let randomIndex = random(0, processedItems.length - 1);
+  const [objectName, setObjectName] = useState(processedItems[randomIndex]);
+
+  const handleClick = () => {
+    randomIndex = random(0, processedItems.length - 1);
+    setObjectName(processedItems[randomIndex]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>I'll sell you this item for 22 Moras</h1>
+        <img
+          src={`https://api.genshin.dev/materials/cooking-ingredients/${objectName}`}
+          alt={objectName}
+        />
+      </div>
+      <h2>
+        {objectName.charAt(0).toUpperCase() +
+          objectName.slice(1).replaceAll("-", " ")}{" "}
+        : 22 Moras
+      </h2>
+      <div>
+        <button onClick={handleClick} type="button" className="button">
+          {" "}
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/Trade.jsx
+++ frontend/src/components/Trade.jsx
@@ -5,13 +5,43 @@ import Sell from "./Sell";
 export default function Trade() {
   const [tradeScreen, setTradeScreen] = useState("presentation");
 
+  const processedItems = [
+    "chilled-meat",
+    "flour",
+    "milk",
+    "pepper",
+    "rice",
+    "salt",
+    "shrimp-meat",
+    "butter",
+    "cream",
+    "ham",
+    "smoked-fowl",
+    "sugar",
+    "bacon",
+    "cheese",
+    "crab-roe",
+    "jam",
+    "sausage",
+  ];
+
+  function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   if (tradeScreen === "presentation") {
     return (
       <PresentationTrade
         tradeScreen={tradeScreen}
         setTradeScreen={setTradeScreen}
+        processedItems={processedItems}
+        random={random}
       />
     );
+  } else if (tradeScreen === "buy") {
+    return <Buy processedItems={processedItems} />;
   }
   return <Sell />;
 }
