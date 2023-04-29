aa9322e765a5b8f887e3439d60cf4fc7ebc9929d
Guillaume Lamourec <parkerpool@live.fr>
2023-04-30 00:30:24 +0200

    updated start page, included storytelling and added player name feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
-import Start from "./components/Start";
+import Start from "./pages/Start";
 import "./App.css";
 import { GatherContextProvider } from "./contexts/GatherContext";
 
 function App() {
+  const [playerName, setPlayerName] = useState("");
+
   return (
     <GatherContextProvider>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Start />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Start playerName={playerName} setPlayerName={setPlayerName} />
+            }
+          />
+          <Route path="/home" element={<Home playerName={playerName} />} />
         </Routes>
       </div>
     </GatherContextProvider>
