19131b0d657f9ccecb069ec1aa52fba2b6580dd1
Yann Richard <yann.richard9@gmail.com>
2023-05-02 09:51:00 +0200

    fixed the CSS problem with combat result modal, and the bug which reseted the PV count in case the player left the combat mode before the end of the fight

--- frontend/src/components/CombatGame.jsx
+++ frontend/src/components/CombatGame.jsx
@@ -11,46 +11,23 @@ export default function CombatGame({
   setInventory,
   moraCount,
   setMoraCount,
+  random,
 }) {
   const {
     enemyChoice,
     result,
-    // setCombatScreen,
     enemyHP,
     setEnemyHP,
     playerHP,
     setPlayerHP,
-    // matchWinner,
     setMatchWinner,
     showCombatResultModal,
     setShowCombatResultModal,
     enemy,
   } = useCombatContext();
 
-  // const dropRarity = 2;
-  //    Il s'agit d'une valeur test, à remplacer par la valeur de la propriété
-  // "rarity" dans l'objet stocké dans le state qui sera alimenté par l'appel à l'API
-
-  const random = (min, max) => {
-    const mini = Math.ceil(min);
-    const maxi = Math.floor(max);
-    return Math.floor(Math.random() * (maxi - mini + 1)) + min;
-  };
   const damage = random(3, 5);
 
-  // const getEnemyHP = (rarity) => {
-  //   if (rarity === 1) {
-  //     setEnemyHP(9);
-  //   } else if (rarity === 2) {
-  //     setEnemyHP(10);
-  //   } else if (rarity === 3) {
-  //     setEnemyHP(11);
-  //   } else if (rarity === 4) {
-  //     setEnemyHP(12);
-  //   }
-  // };
-
-  // getEnemyHP(dropRarity);
   const ongoingMatch = playerHP > 0 && enemyHP > 0;
 
   useEffect(() => {
@@ -70,7 +47,6 @@ export default function CombatGame({
       setShowCombatResultModal(true);
     }
   });
-  // const winner = playerHP === 0 || enemyHP === 0;
 
   return (
     <div className={styles.combatGame}>
@@ -119,4 +95,5 @@ CombatGame.propTypes = {
   setInventory: PropTypes.func.isRequired,
   moraCount: PropTypes.number.isRequired,
   setMoraCount: PropTypes.func.isRequired,
+  random: PropTypes.func.isRequired,
 };
